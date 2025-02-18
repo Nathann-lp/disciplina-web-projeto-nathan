4ebd95617e66e21ab880182dff99502f40560d17
Nathann-lp <90117733+Nathann-lp@users.noreply.github.com>
2025-02-17 21:13:57 -0300

    Add files via upload

---
+++ src/main/java/br/ueg/desenvolvimento/web/projetonathan/ProjetonathanApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projetonathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetonathanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetonathanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/desenvolvimento/web/projetonathan/ProjetonathanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.desenvolvimento.web.projetonathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetonathanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
